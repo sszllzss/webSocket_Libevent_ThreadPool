ff1944054a42e33152ea7b86bec0dcdc27a7677f
sszllzss <sszllzss@foxmail.com>
2018-09-19 00:23:52 +0800

    Threadpool.cpp
    完善线程次

--- evbase_threadpool.h
+++ evbase_threadpool.h
@@ -4,10 +4,19 @@
 # > Mail: sszllzss@foxmail.com
 # > Blog: sszlbg.cn
 # > Created Time: 2018-09-17 22:10:26
-# > Revise Time: 2018-09-17 22:10:28
+# > Revise Time: 2018-09-19 00:13:04
  ************************************************************************/
 
 #ifndef _EVBASE_THREADPOOL_H
 #define _EVBASE_THREADPOOL_H
- 
+#include<event.h>
+#include<event2/buffer.h>
+#include<event2/bufferevent.h>
+#include<event2/event.h>
+#include<event2/event-config.h>
+typedef struct evbase_threadpool_t  evbase_threadpool_t;
+evbase_threadpool_t *evbase_threadpool_new(unsigned base_max_monitor_num,unsigned min_thread_num);
+void evbase_threadpool_destroy(evbase_threadpool_t * evb_thpool);
+event_base * evbase_threadpool_add_event(evbase_threadpool_t * evb_thpool);
+void evbase_threadpool_close_event(evbase_threadpool_t *evb_thpool ,event_base * base);
 #endif
