94b543e6f8cce9ee336caad888b253722b9efacc
sszllzss <sszllzss@foxmail.com>
2018-09-19 22:34:37 +0800

    测试线程池

--- test_thpool.c
+++ test_thpool.c
@@ -1,10 +1,10 @@
 /*************************************************************************
-# > File Name: thpool.c
+# > File Name: test_thpool.c
 # > Author: SSZL
 # > Mail: sszllzss@foxmail.com
 # > Blog: sszlbg.cn
 # > Created Time: 2018-09-17 15:09:35
-# > Revise Time: 2018-09-17 16:47:06
+# > Revise Time: 2018-09-19 20:17:06
  ************************************************************************/
 
 #include<stdio.h>
@@ -13,6 +13,7 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<string.h>
+#include"evbase_threadpool.h"
 #include"Threadpool.h"
 /* 线程池中的线程，模拟处理业务 */
 void *process(void *arg)
@@ -27,13 +28,13 @@ void *process(void *arg)
 }
 int main(void)
 {
-    /*threadpool_t *threadpool_create(int min_thr_num, int max_thr_num, int queue_max_size);*/
-
-    threadpool_t *thp = threadpool_create(10);/*创建线程池*/
+    threadpool_t *thp = threadpool_create(10);
+     /* evbase_threadpool_t *ev_thpool = evbase_threadpool_new(1000, 10); */ 
+    /* threadpool_t *thp = evbase_threadpool_get_threadpool(ev_thpool) ;*/
     printf("pool inited\r\n");
 
     int num[20000], i;
-    for (i = 0; i < 20000; i++) {
+    for (i = 0; i < 20; i++) {
         num[i]=i;
         printf("add task %d\n",i);
         threadpool_add(thp, process, (void*)&num[i]);     /* 向线程池中添加任务 */
